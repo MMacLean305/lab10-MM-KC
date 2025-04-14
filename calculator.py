8ce3289ef65c4706b622e414e853b792ff8e8b06
Matthew <matthew.thomas.maclean@gmail.com>
2025-04-14 17:11:58 -0400

    fixed

--- calculator.py
+++ calculator.py
@@ -6,7 +6,7 @@ import math
 def add(a, b):
     return a+b
 
-def sub(a, b):
+def subtract(a, b):
     return a-b
 
 def mul(a, b):
@@ -18,7 +18,7 @@ def div(a, b): # raise ZeroDivisionError if a == 0
     else:
         return b / a
 
-def log(a, b):
+def logarithm(a, b):
     if a <= 0 or a == 1 or b <= 0:
         raise ValueError()
     return math.log(b, a)
@@ -27,13 +27,9 @@ def exp(a, b):
     return a**b
 
 def square_root(a):
-    try:
-        if a < 0:
-            raise ValueError()
-        return math.sqrt(a)
-
-    except ValueError as e:
-        print(f"Error: {e}")
+    if a < 0:
+        raise ValueError()
+    return math.sqrt(a)
 
 def hypotenuse(a, b):
     return math.hypot(a, b)
